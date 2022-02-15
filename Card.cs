654f867a8b623a178b080018e679f32226c259e7
Grumpylama <65866382+Grumpylama@users.noreply.github.com>
2022-02-15 22:18:12 +0100

    Deck Functionality

--- Card.cs
+++ Card.cs
@@ -3,28 +3,72 @@ using System.Collections.Generic;
 
 namespace poker
 {
-    public class card 
+
+    public class Deck
     {
-        public static List<card> GetCards()
+        public void ResetDeck(int amount)
         {
 
-            return new List<card>();
+            Cards.Clear();
+            for(int y = 1; y <= amount; y++)
+            {
+                for (int i = 1; i <= 13; i++)
+                {
+                    Cards.Add(new HCard(i));
+                }
+                for (int i = 1; i <= 13; i++)
+                {
+                    Cards.Add(new CCard(i));
+                }
+                for (int i = 1; i <= 13; i++)
+                {
+                    Cards.Add(new SCard(i));
+                }
+                for (int i = 1; i <= 13; i++)
+                {
+                    Cards.Add(new DCard(i));
+                }
+            }
+
         }
 
-        public card()
+        public List<Card> Cards;
+
+
+    }
+    public class Card 
+    {
+        public Card(int number)
+        {
+            this.number = number;
+        }
+        int number;
+
+        public void Draw()
         {
 
         }
-
-        
     }
 
-    public class HCard : card 
+    public class HCard : Card 
     {
-
+        public HCard(int value) : base(value) { }
     }
 
+    public class CCard : Card
+    {
+        public CCard(int value) : base(value) { }
+    }
 
+    public class SCard : Card
+    {
+        public SCard(int value) : base(value) { }
+    }
+    
+    public class DCard : Card
+    {
+        public DCard(int value) : base(value) { }
+    }
 }
 
 
