5c5ea2c2f476807ac09ccf40f731f4f27a8221e7
Grumpylama <65866382+Grumpylama@users.noreply.github.com>
2022-02-15 21:55:14 +0100

    Wowcard

--- Card.cs
+++ Card.cs
@@ -1,11 +1,31 @@
 using System;
+using System.Collections.Generic;
 
 namespace poker
 {
     public class card 
     {
+        public static List<card> GetCards()
+        {
 
+            return new List<card>();
+        }
+
+        public card()
+        {
+
+        }
+
+        
+    }
+
+    public class HCard : card 
+    {
 
     }
 
+
 }
+
+
+
