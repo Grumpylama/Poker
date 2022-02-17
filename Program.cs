74138234e129c0841b335780baa5f5fc6799cc4b
Grumpylama <65866382+Grumpylama@users.noreply.github.com>
2022-02-17 20:18:58 +0100

    New cards :)

--- Program.cs
+++ Program.cs
@@ -7,33 +7,16 @@ namespace poker
     { 
         static void Main(string[] args)
         {
-            Console.WriteLine("♠, ♦, ♥, ♣");
-            List<string> lines = new List<string>();
-            lines.Add("┌─────────┐");
-            lines.Add("│3░░░░░░░░│");
-            lines.Add("│░░░░░░░░░│");
-            lines.Add("│░░░░♠░░░░│");
-            lines.Add("│░░░░░░░░░│");
-            lines.Add("│░░░░░░░░3│");
-            lines.Add("└─────────┘");
-
-            List<string> line = new List<string>();
-            line.Add("┌─────────┐");
-            line.Add("│3        │");
-            line.Add("│         │");
-            line.Add("│         │");
-            line.Add("│         │");
-            line.Add("│        3│");
-            line.Add("└─────────┘");
-            foreach (string lin in lines)
+            Deck deck = new Deck();
+            deck.ResetDeck();
+            foreach (Card c in deck.Cards)
             {
-                Console.WriteLine(lin);
+                foreach (string s in c.lines)
+                {
+                    Console.WriteLine(s);
+                }
             }
-            Console.WriteLine();
-            foreach (string liness in line)
-            {
-                Console.WriteLine(liness);
-            }
+            
         }
     }
 }
